630132d903b733d25082cc15398f36cbc232227f
vikrav14 <vikrav14@gmail.com>
2026-08-23 23:04:47 +0400

    test(care): assert hidden reminder backbone

--- gateway/test/care-reminders-backbone.test.js
+++ gateway/test/care-reminders-backbone.test.js
@@ -5,11 +5,13 @@ const {
   SERVICE_CONTRACT,
 } = require('../src/service-backbones/care-reminders');
 
-test('care-reminders remains a disabled service backbone', () => {
-  assert.equal(SERVICE_CONTRACT.lifecycle, 'backbone');
+test('care-reminders remains disabled and customer-hidden', () => {
+  assert.equal(SERVICE_CONTRACT.lifecycle, 'implementation-disabled');
   assert.equal(SERVICE_CONTRACT.enabledByDefault, false);
+  assert.equal(SERVICE_CONTRACT.customerVisible, false);
   assert.equal(SERVICE_CONTRACT.minimumPlan, 'care');
   assert.ok(SERVICE_CONTRACT.protocolCommands.length > 0);
+  assert.deepEqual(SERVICE_CONTRACT.acceptedProtocolCommands, []);
   assert.ok(SERVICE_CONTRACT.backendMilestones.length > 0);
   assert.ok(SERVICE_CONTRACT.frontendMilestones.length > 0);
   assert.ok(SERVICE_CONTRACT.acceptanceGates.length > 0);
