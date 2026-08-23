5003a427a481573d30221f3437acdae06b7083de
vikrav14 <vikrav14@gmail.com>
2026-08-23 19:08:44 +0400

    fix(care): remove redundant Flutter test import

--- apps/mobile/test/wellbeing_service_test.dart
+++ apps/mobile/test/wellbeing_service_test.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:guardian/services/guardian_entitlements.dart';
 import 'package:guardian/services/guardian_services.dart';
 
 GuardianSubscription plan(String value) => GuardianSubscription.fromMap({
